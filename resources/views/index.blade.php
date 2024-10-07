0578651364a95705c86d75776740f363580017a3
Dmoonwalker <fadlullahbugaje@gmail.com>
2024-10-07 13:46:03 +0100

    version 0.12.3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -219,8 +219,16 @@
             </div>
         </div>
         <!-- Right Column -->
-        <div class="right-column">
-        <script src="//servedby.eleavers.com/ads/ads.php?t=MzA5MjU7MjEwMTQ7c3F1YXJlLm1lZGl1bV9yZWN0YW5nbGU=&index=1"></script>
+        <div class="right-column"><script type="text/javascript">
+	atOptions = {
+		'key' : '711606b17b97994069c24795c934118a',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/711606b17b97994069c24795c934118a/invoke.js"></script>
             <!-- Insert your content or advertisements here -->
         </div>
     </div>
